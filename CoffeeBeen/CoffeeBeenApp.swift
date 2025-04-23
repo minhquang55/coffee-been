80fd0711669d4e79b60e1596af12f52905bcbc95
quangtm <quangtm@kaopiz.com>
2025-04-23 14:51:20 +0700

    Initial Commit

---
+++ CoffeeBeen/CoffeeBeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBeenApp.swift
+//  CoffeeBeen
+//
+//  Created by Trần Minh Quang on 23/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeBeen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeBeen
+//
+//  Created by Trần Minh Quang on 23/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
